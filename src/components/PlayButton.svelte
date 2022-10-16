65d756e3f6928e7a670082434f488ae5618ce5dc
Simon Mitternacht <simon.mitternacht@gmail.com>
2022-10-16 11:53:23 +0200

    Add missing file.

---
+++ src/components/PlayButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a {href} property="url" aria-label="Play song on spotify">
+  <i aria-hidden="true">&#xf144;</i>
+</a>
+
+<style>
+  a {
+    text-decoration: none;
+  }
+  i {
+    font-family: "Font Awesome 6 Free";
+    font-weight: 900;
+    font-style: normal;
+  }
+</style>
