227bfb202f42ccea7766fd48854dd58e0b88da3d
Simon Mitternacht <simon.mitternacht@gmail.com>
2025-08-28 20:20:18 +0200

    Remove last Svelte components. Make calendar visible.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mariadue.net",
-  integrations: [svelte(), sitemap()],
+  integrations: [sitemap()],
   trailingSlash: "always",
+  output: 'static',
 });
